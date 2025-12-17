0e80145313151854d912eb4415c6ca2ed1bea464
souzza <cauelima752@gmail.com>
2025-12-17 16:02:05 -0300

    Animando tudo com gsap (mobile, notebook, desktop)

--- animation.js
+++ animation.js
@@ -98,38 +98,197 @@ heroTl
 gsap.delayedCall(0.2, () => {
   ScrollTrigger.refresh();
 });
+
 // SEPARAÇÃO DA ANIMAÇÃO DA HERO E DAS DEMAIS ANIMAÇÕES DO SITE
 
-gsap.from(".cruzVerde", {
-  rotate: 90,
-  scrollTrigger: {
-    trigger: ".hero",
-    start: "top 30%",
-    end: "bottom 30%",
-    scrub: 2,
-    markers: false,
-  },
+const mm = gsap.matchMedia();
+
+mm.add("(max-width: 768px)", () => {
+  gsap.fromTo(
+    ".cruzVerde",
+    { rotate: 90, y: 0 },
+    {
+      rotate: 0,
+      y: 300,
+      scrollTrigger: {
+        trigger: ".hero h2",
+        start: "top 0%",
+        end: "bottom -40%",
+        scrub: 2,
+        markers: false,
+      },
+    }
+  );
+
+  gsap.from(".estrelaDourada", {
+    rotate: 90,
+    y: 200,
+    scrollTrigger: {
+      trigger: ".hero h2",
+      start: "top 0%",
+      end: "bottom -40%",
+      scrub: 2,
+      markers: false,
+    },
+  });
+
+  gsap.to(".brilhoVerde", {
+    y: 300,
+    scrollTrigger: {
+      trigger: ".hero h2",
+      start: "top 0%",
+      end: "bottom -40%",
+      scrub: 2,
+      markers: false,
+    },
+  });
+
+  gsap.from(".work, .artist, .action, .art, .profile", {
+    opacity: 0,
+    filter: "blur(5px)",
+    y: 10,
+    stagger: 0.2,
+    duration: 0.6,
+    scrollTrigger: {
+      trigger: ".stats",
+      start: "top 50%",
+      markers: false,
+    },
+  });
+    
+    gsap.utils.toArray(".pessoas .box").forEach((box) => {
+      gsap.from(box, {
+        opacity: 0,
+        y: 20,
+        duration: 0.8,
+        filter: "blur(6px)",
+        scrollTrigger: {
+          trigger: box,
+          start: "top 70%",
+          toggleActions: "play none none none",
+        },
+      });
+    });
+
+    gsap.from(".dois", {
+      y: 30,
+      opacity: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: ".dois",
+        start: "top 40%",
+        markers: false,
+      },
+    });
 });
 
-gsap.to(".cruzVerde", {
-  y: 200,
-  scrollTrigger: {
-    trigger: ".hero",
-    start: "top 30%",
-    end: "bottom 30%",
-    scrub: 2,
-    markers: false,
-  },
+// SEPARAÇÃO DAS ANIMAÇÕES EM DIFERENTES TELAS
+
+mm.add("(min-width: 769px) and (max-width: 1366px)", () => {
+  gsap.fromTo(
+    ".cruzVerde",
+    { rotate: 90, y: 0 },
+    {
+      rotate: 0,
+      y: 200,
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top 30%",
+        end: "bottom 30%",
+        scrub: 2,
+        markers: false,
+      },
+    }
+  );
+
+  gsap.from(".estrelaDourada", {
+    rotate: 90,
+    y: 200,
+    scrollTrigger: {
+      trigger: ".hero",
+      start: "top 30%",
+      end: "bottom 30%",
+      scrub: 2,
+      markers: false,
+    },
+  });
+
+  gsap.from(".work, .artist, .action, .art, .profile", {
+    opacity: 0,
+    filter: "blur(5px)",
+    y: 10,
+    stagger: 0.2,
+    duration: 0.6,
+    scrollTrigger: {
+      trigger: ".stats",
+      start: "top 50%",
+      markers: false,
+    },
+  });
+    
+    gsap.from(".pessoas", {
+        y: 30,
+        opacity: 0,
+        duration:1,
+        scrollTrigger: {
+            trigger: ".pessoas",
+            start: "top 40%",
+            markers:true
+        }
+    })
 });
 
-gsap.from(".estrelaDourada", {
-  rotate: 90,
-  y: 200,
-  scrollTrigger: {
-    trigger: ".hero",
-    start: "top 30%",
-    end: "bottom 30%",
-    scrub: 2,
-    markers: false,
-  },
+// SEPARAÇÃO DAS ANIMAÇÕES EM DIFERENTES TELAS
+
+mm.add("(min-width: 1367px) and (max-width: 1930px)", () => {
+  gsap.fromTo(
+    ".cruzVerde",
+    { rotate: 90, y: 0 },
+    {
+      rotate: 0,
+      y: 0,
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top 30%",
+        end: "bottom 30%",
+        scrub: 2,
+        markers: false,
+      },
+    }
+  );
+  gsap.from(".estrelaDourada", {
+    rotate: 90,
+    y: 200,
+    scrollTrigger: {
+      trigger: ".hero",
+      start: "top 30%",
+      end: "bottom 30%",
+      scrub: 2,
+      markers: false,
+    },
+  });
+
+  gsap.from(".work, .artist, .action, .art, .profile", {
+    opacity: 0,
+    filter: "blur(5px)",
+    y: 10,
+    stagger: 0.2,
+    duration: 0.6,
+    scrollTrigger: {
+      trigger: ".stats",
+      start: "top 50%",
+      markers: false,
+    },
+  });
+    
+    gsap.from(".pessoas", {
+      y: 30,
+      opacity: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: ".pessoas",
+        start: "top 40%",
+        markers: false,
+      },
+    });
 });
